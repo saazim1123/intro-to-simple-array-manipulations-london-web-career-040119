e804c8b3254fa5cf99e83565c2ba43042bea1b9e
Adnan Azim <saazim1123@gmail.com>
2019-03-11 00:56:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,6 +18,21 @@ def pop_with_args(array)
   array.pop(2) 
 end 
 
-def shift(array)
-  
+def using_shift(array)
+  array.shift
 end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1
+end
+
+
+
+
+
+
+
